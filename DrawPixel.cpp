ecc03c2302774849cc6bad959646e8d563742e49
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-05-21 13:42:06 +0900

    弾が打てないバグを修正(関数呼び出しミス(GetGraphになってなかった))

--- DrawPixel.cpp
+++ DrawPixel.cpp
@@ -4,10 +4,11 @@
 
 #define SHOTSIZE 10
 
-void UpdatePlayer(Player* player, std::vector<Shot>& shots,int& shotBflag);
+void UpdatePlayer(Player* player, std::vector<Shot*> shots,int& shotBflag);
 void UpdateEnemy(Enemy* enemy);
-void UpdateShotState(Player* player,Shot& shot);
-void UpdateShotView(Shot& shot);
+void UpdateShotState(Player* player,Shot* shot);
+void UpdateShotView(Shot* shot);
+void HitCheck(Enemy* enemy, std::vector<Shot*> shots);
 
 
 //プログラムはWinMainから始まる。
@@ -26,7 +27,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 
 	Enemy* enemy = new Enemy(0, 50, LoadGraph("img/EnemyImg.png"), 1);
 
-	std::vector<Shot> shots(SHOTSIZE, Shot(0, 0, LoadGraph("img/Shot.png"), 0));
+	std::vector<Shot*> shots;
+	for (int i = 0; i < SHOTSIZE; i++) {
+		shots.push_back(new Shot(0, 0, LoadGraph("img/Shot.png"), 0));
+	}
 	int shotBflag = 0;
 
 	while (1) {
@@ -36,6 +40,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 		UpdatePlayer(player, shots, shotBflag);
 
 		UpdateEnemy(enemy);
+
+		HitCheck(enemy, shots);
 
 		ScreenFlip();
 
@@ -48,14 +54,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 	return 0;
 }
 
-void UpdatePlayer(Player* player, std::vector<Shot>& shots,int& ShotBFlag) {
+void UpdatePlayer(Player* player, std::vector<Shot*> shots, int& ShotBFlag) {
 
 	if (CheckHitKey(KEY_INPUT_UP) == 1) player->setY(player->getY() - 3);
 	if (CheckHitKey(KEY_INPUT_DOWN) == 1) player->setY(player->getY() + 3);
 	if (CheckHitKey(KEY_INPUT_LEFT) == 1) player->setX(player->getX() - 3);
 	if (CheckHitKey(KEY_INPUT_RIGHT) == 1) player->setX(player->getX() + 3);
 
-	if (player->getX() < 0) player->setX(0); 
+	if (player->getX() < 0) player->setX(0);
 	if (player->getX() > 640 - 64) player->setX(640 - 64);
 
 	if (player->getY() < 0) player->setY(0);
@@ -66,8 +72,8 @@ void UpdatePlayer(Player* player, std::vector<Shot>& shots,int& ShotBFlag) {
 	if (CheckHitKey(KEY_INPUT_SPACE) == 1) {
 		if (ShotBFlag == 0) {
 			for (size_t i = 0; i < shots.size(); i++) {
-				if (shots[i].getFlag() == 0) {
-					UpdateShotState(player,shots[i]);
+				if (shots[i]->getFlag() == 0) {
+					UpdateShotState(player, shots[i]);
 					break;
 				}
 			}
@@ -79,33 +85,33 @@ void UpdatePlayer(Player* player, std::vector<Shot>& shots,int& ShotBFlag) {
 	}
 
 	for (size_t i = 0; i < shots.size(); i++) {
-		if (shots[i].getFlag() == 1) {
+		if (shots[i]->getFlag() == 1) {
 			UpdateShotView(shots[i]);
 		}
 	}
 }
 
-void UpdateShotState(Player* player, Shot& shot) {
+void UpdateShotState(Player* player, Shot* shot) {
 	int Pw, Ph, Sw, Sh;
 
 	GetGraphSize(player->getGraph(), &Pw, &Ph);
-	GetGraphSize(shot.getGraph(), &Sw, &Sh);
+	GetGraphSize(shot->getGraph(), &Sw, &Sh);
 
-	shot.setX((Pw - Sw) / 2 + player->getX());
-	shot.setY((Ph - Sh) / 2 + player->getY());
+	shot->setX((Pw - Sw) / 2 + player->getX());
+	shot->setY((Ph - Sh) / 2 + player->getY());
 
-	shot.setFlag(1);
+	shot->setFlag(1);
 }
 
 
-void UpdateShotView(Shot& shot) {
-	if (shot.getFlag() == 1) {
-		shot.setY(shot.getY() - 16);
-		if (shot.getY() < -80) {
-			shot.setFlag(0);
+void UpdateShotView(Shot* shot) {
+	if (shot->getFlag() == 1) {
+		shot->setY(shot->getY() - 16);
+		if (shot->getY() < -80) {
+			shot->setFlag(0);
 		}
 
-		DrawGraph(shot.getX(), shot.getY(), shot.getFlag(), TRUE);
+		DrawGraph(shot->getX(), shot->getY(), shot->getGraph(), TRUE);
 	}
 }
 
@@ -130,4 +136,23 @@ void UpdateEnemy(Enemy* enemy) {
 	}
 
 	DrawGraph(enemy->getX(), enemy->getY(), enemy->getGraph(), TRUE);
+}
+
+void HitCheck(Enemy* enemy, std::vector<Shot*> shots) {
+	int EneW, EneH, SW, SH;
+	GetGraphSize(enemy->getGraph(), &EneW, &EneH);
+	GetGraphSize(shots[0]->getGraph(), &SW, &SH);
+
+	for (auto shot : shots) {
+		if (shot->getFlag() == 1) {
+			if (((shot->getX() > enemy->getX() && shot->getX() < enemy->getX()+ EneW) ||
+				(enemy->getX() > shot->getX() && enemy->getX() < shot->getX() + SW)) &&
+				((shot->getY() > enemy->getY() && shot->getY() < enemy->getY() + EneH) ||
+				(enemy->getY() > shot->getY() && enemy->getY() < shot->getY() + SH))) {
+
+				//接触した場合弾は消える
+				shot->setFlag(0);
+			}
+		}
+	}
 }
