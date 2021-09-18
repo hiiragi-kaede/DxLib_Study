fc0e8cad31747999437a2497a476bdaa9a1ba145
hiiragi-kaede <zoro18782ryt131@gmail.com>
2021-09-18 12:20:10 +0900

    タイトル画面の選択機能を実装

--- GameMaster.cpp
+++ GameMaster.cpp
@@ -2,27 +2,40 @@
 #include"DxLib.h"
 #include"Player.hpp"
 #include"Enemy.hpp"
+#include"string"
 
 static const int TitleFont = CreateFontToHandle(DX_FONTTYPE_NORMAL, 40, 3);
 static const unsigned Cr_white = GetColor(255, 255, 255);
+static const unsigned Cr_orange = GetColor(255, 45, 00);
 static master::GameState gameState = master::GameState::Title;
+static const int OptionSize = 3;
+static const std::string OptionStr[OptionSize] = { "Play","Config","Exit Game" };
+static const int OptionPos[2] = { 100,200 };
+static const int OptionDis = 20;
+static int SelectedOption = 0;
+static const int WaitTimeMS = 150;
+static int StartTime = 0;
 
 void master::drawTitleScene()
 {
 	SetFontSize(40);
 	DrawString(100, 120, "Test Title", Cr_white);
+	master::drawTitleOptions();
+}
+
+void master::drawTitleOptions()
+{
 	SetFontSize(15);
-	DrawString(100, 200, "Play", Cr_white);
-	DrawString(100, 220, "Config", Cr_white);
+	for (int i = 0; i < OptionSize; i++) {
+		unsigned color = (i == SelectedOption) ? Cr_orange : Cr_white;
+		DrawString(OptionPos[0], OptionPos[1] + OptionDis * i, OptionStr[i].c_str(), color);
+	}
 }
 
 void master::Update()
 {
-	if (CheckHitKey(KEY_INPUT_SPACE)) {
-		SetGameState(master::GameState::Game);
-		pl::Instantiate();
-		ene::Instantiate();
-	}
+	master::CheckGameStart();
+	master::CheckTitleSelect();
 }
 
 master::GameState master::GetGameState()
@@ -33,5 +46,30 @@ master::GameState master::GetGameState()
 void master::SetGameState(master::GameState state)
 {
 	gameState = state;
+}
+
+void master::CheckGameStart()
+{
+	if (CheckHitKey(KEY_INPUT_SPACE)) {
+		SetGameState(master::GameState::Game);
+		pl::Instantiate();
+		ene::Instantiate();
+	}
+}
+
+void master::CheckTitleSelect()
+{
+	if (GetNowCount() - StartTime > WaitTimeMS) {
+		if (CheckHitKey(KEY_INPUT_DOWN)) {
+			SelectedOption = (SelectedOption + 1) % OptionSize;
+			StartTime = GetNowCount();
+		}
+		else if (CheckHitKey(KEY_INPUT_UP)) {
+			SelectedOption--;
+			SelectedOption = (SelectedOption < 0) ?
+							SelectedOption + OptionSize : SelectedOption;
+			StartTime = GetNowCount();
+		}
+	}
 }
 
--- GameMaster.hpp
+++ GameMaster.hpp
@@ -3,7 +3,10 @@
 namespace master {
 	enum class GameState { Title, Game };
 	void drawTitleScene();
+	void drawTitleOptions();
 	void Update();
 	GameState GetGameState();
 	void SetGameState(master::GameState state);
+	void CheckGameStart();
+	void CheckTitleSelect();
 }
